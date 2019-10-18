cfb17fc3effac1ec6a37b366edffbee599573380
Alice Freda <alicefreda@gmail.com>
2019-10-18 21:27:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,21 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 
-def key_for_min_value(hash)
-  # min_value = 10000
-  min_value = nil
-  min_key = nil
-  hash.each do |key, value|
-      if min_value == nil || value < min_value
-        min_value = value
-        min_key = key
-      end
-  end
-  min_key
-end
-
 # def key_for_min_value(hash)
-#   hash.reduce do |item1, item2|
-#     item1.last < item2.last ? item1 : item2
+#   # min_value = 10000
+#   min_value = nil
+#   min_key = nil
+#   hash.each do |key, value|
+#       if min_value == nil || value < min_value
+#         min_value = value
+#         min_key = key
+#       end
 #   end
+#   min_key
 # end
+
+def key_for_min_value(hash)
+  hash.reduce do |item1, item2|
+    min_item = item1.last < item2.last ? item1 : item2
+    min_item[0]
+  end
+end
