57e4ca4568f0567a1ab8384aaef1202a7b7e6469
Alice Freda <alicefreda@gmail.com>
2019-10-18 21:34:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,8 +18,6 @@ require 'pry'
 def key_for_min_value(hash)
   hash.reduce do |item1, item2|
     min_item = item1.last < item2.last ? item1 : item2
-    key = min_item[0]
-    key
       binding.pry
   end
 
